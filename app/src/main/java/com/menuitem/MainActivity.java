c94fa9269893f9233b142947a5a470c7ff992d7e
小欢欢 <1401518768@qq.com>
2017-08-08 17:59:17 +0800

    first

--- app/src/main/java/com/menuitem/MainActivity.java
+++ app/src/main/java/com/menuitem/MainActivity.java
@@ -2,6 +2,7 @@ package com.menuitem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int score[] = {67, 69, 75, 87, 89, 90, 99, 100};
+        for (int i = 0; i < score.length - 1; i++) {//7
+            for (int j = 0; j < score.length - i - 1; j++) {//7 6
+                if (score[j] < score[j + 1]) {
+                    int temp = score[j];
+                    score[j] = score[j + 1];
+                    score[j + 1] = temp;
+                }
+                //69 67 75 87 89 90 99 100
+                //69 75 67 87 89 90 99 100
+                //69 75 87 67 89 90 99 100
+                //69 75 87 89 67 90 99 100
+                //69 75 87 89 90 67 99 100
+                //69 75 87 89 90 99 67 100
+                //69 75 87 89 90 99 100 67
+                for (int a = 0; a < score.length; a++) {
+//                    Log.e("df", "排序..：" + j + score[a]);
+                }
+            }
+            // 69 75 87 89 90 99 100 67
+            // 75 87 89 90 99 100 69 67
+            // 87 89 90 99 100 75 69 67
+            // 89 90 99 100 87 75 69 67
+            // 90 99 100 89 87 75 69 67
+            // 99 100 90 89 87 75 69 67
+            // 100 99 90 89 87 75 69 67
+            for (int a = 0; a < score.length; a++) {
+//                Log.e("df", "排序..：" + score[a] );
+            }
+        }
+        for (int a = 0; a < score.length; a++) {
+//            Log.e("df", "排序之后：" + score[a] );
+        }
     }
 }
